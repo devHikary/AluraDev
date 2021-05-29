fac5661fc61936e64b54aeb256b2cee2e8eafc32
devHikary <wrkkarina@gmail.com>
2021-05-28 23:41:59 -0300

    Localstorage implementado

---
+++ js/comunidade.js
@@ -0,0 +1,55 @@
+const listaProjetos = document.querySelector('.quadros')
+
+new function () {
+  mostraProjetos()
+}
+
+function mostraProjetos() {
+  if (localStorage.length <= 0) {
+    return
+  }
+  let projetos = []
+  for (let i = 0; i < localStorage.length; i++) {
+    projetos.push(JSON.parse(localStorage.getItem(i)))
+  }
+  projetos.forEach(projeto => {
+    const cartao = criaCartao(projeto)
+    listaProjetos.innerHTML += cartao
+    const codigoHtml = listaProjetos.querySelector(`[data-id="${projeto.id}"]`)
+    codigoHtml.querySelector('code').innerText = projeto.detalhesDoProjeto.codigo
+    
+    const codigo = codigoHtml.querySelector('code')
+    hljs.highlightBlock(codigo)
+  })
+}
+
+function criaCartao(projeto) {
+  const cartao = `
+    <div class="quadro-container">
+          <div class="quadro" id="quadroColor">
+            <div class="editor">
+              <div class="dot-container">
+                <div class="dot macRed"></div>
+                <div class="dot macYellow"></div>
+                <div class="dot macGreen"></div>
+              </div>
+              <div class="codigo" data-id="${projeto.id}">
+                <code class="fonteCodigo hljs ${projeto.detalhesDoProjeto.linguagem}"></code>
+              </div>
+            </div>
+          </div>
+          <h2 class="tituloProjeto">${projeto.detalhesDoProjeto.nomeDoProjeto}</h2>
+          <p class="descricaoProjeto">${projeto.detalhesDoProjeto.descricaoDoProjeto}</p>
+          <div class="icone-container">
+            <img class="iconeComentaLike" src="./img/comunidade/iconeComentario.png">
+            <p class="numero">9</p>
+            <img class="iconeComentaLike" src="./img/comunidade/iconeLike.png">
+            <p class="numero">9</p>
+            <div  class="autor-container">
+              <img class="iconeComentaLike" src="./img/comunidade/iconeAutor.png">
+              <p class="numero">@Harry</p>
+            </div>
+          </div>
+    `
+  return cartao
+}
--- js/index.js
+++ js/index.js
@@ -1,27 +1,58 @@
 const areaDoCodigo = document.querySelector('.codigo')
 const linguagem = document.querySelector('.selectLinguagem')
-const botao = document.querySelector('.botaoVisualizar')
+const botaoPreview = document.querySelector('.botaoVisualizar')
+
+const tituloProjeto = document.querySelector('.inputNomeProjeto')
+const descricaoProjeto = document.querySelector('.inputDescricao')
+const botaoSalvar = document.querySelector('.botaoSalvar')
 
 const inputColor = document.querySelector('[data-color]');
 
-inputColor.addEventListener('input', (evento)=>{
+inputColor.addEventListener('input', (evento) => {
 
   evento.preventDefault();
   const valor = inputColor.value;
-  document.getElementById('quadroColor').style.backgroundColor = valor; 
+  document.getElementById('quadroColor').style.backgroundColor = valor;
 })
 
 function mudaLinguagem() {
-    const codigo = areaDoCodigo.querySelector('code')
-    areaDoCodigo.innerHTML = `<code class="fonteCodigo hljs ${linguagem.value}" contenteditable="true" aria-label="editor"></code>`
-    areaDoCodigo.firstChild.innerText = codigo.innerText
+  const codigo = areaDoCodigo.querySelector('code')
+  areaDoCodigo.innerHTML = `<code class="fonteCodigo hljs ${linguagem.value}" contenteditable="true" aria-label="editor"></code>`
+  areaDoCodigo.firstChild.innerText = codigo.innerText
 }
 
 linguagem.addEventListener('change', () => {
-    mudaLinguagem()
+  mudaLinguagem()
 })
 
-botao.addEventListener('click', () => {
-    const codigo = areaDoCodigo.querySelector('code')
-    hljs.highlightBlock(codigo)
+botaoPreview.addEventListener('click', () => {
+  const codigo = areaDoCodigo.querySelector('code')
+  hljs.highlightBlock(codigo)
 })
+
+botaoSalvar.addEventListener('click', () => {
+  if (typeof(Storage) !== "undefined") {
+      console.log('Yay, support!')
+      const projeto = montaProjeto()
+      salvaLocalStorage(projeto)
+  } else {
+      console.log('Nay, no support!')
+  }
+})
+
+function montaProjeto() {
+  let projeto = {
+    'id': localStorage.length,
+    'detalhesDoProjeto': {
+      'nomeDoProjeto': tituloProjeto.value,
+      'descricaoDoProjeto': descricaoProjeto.value,
+      'linguagem': linguagem.value,
+      'codigo': areaDoCodigo.querySelector('code').innerText
+    }
+  }
+  return projeto
+}
+
+function salvaLocalStorage(objetoJson) {
+  localStorage.setItem(objetoJson.id, JSON.stringify(objetoJson))
+}
